18ecc7f75b1b97d5bf2851f8831bbd93a3c60442
Luke Davis <lukedavis@google.com>
2019-01-16 09:20:59 -0800

    Move examples to their own repo

--- examples/functions/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require('firebase-functions');
-const { app } = require('./app');
-
-console.log(functions);
-console.log('---|---');
-
-exports.leviathanExample = functions.https.onRequest(app.app);
